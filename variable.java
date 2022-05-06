cff675a706f7ca4229435231078ab1dfdf5da7c8
Shaik Yasmin <104405617+shaik-yasmin@users.noreply.github.com>
2022-05-06 10:20:23 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,17 @@
+//to check all variables are equal or not
+import java.util.Scanner;
+class variable
+  {
+    public static void main(String args[])
+    {
+      int a=10,b=10,c=10;
+      if(a==b&&b==c&&c==a)
+      {
+        System.out.println("all variables are equal");
+      }
+      else
+      {
+     System.out.println("all variables are not equal");
+      }
+    }
+  }
